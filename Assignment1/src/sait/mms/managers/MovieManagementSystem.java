4af591cadf08a380a6e7b7744c4bbb5792addbd4
Baodu <Baodu@DESKTOP-VVVDRG1>
2022-06-08 01:19:08 -0600

    asd

---
+++ Assignment1/src/sait/mms/managers/MovieManagementSystem.java
@@ -0,0 +1,33 @@
+package sait.mms.managers;
+
+public class MovieManagementSystem {
+	
+	public MovieManagementSystem()
+            throws java.io.FileNotFoundException {
+		
+	}
+		
+		public void addMovie​(int duration, java.lang.String title, int year) {
+		
+		}
+	
+		public void displayMenu() {
+		
+		}
+		
+		public void generateMoviesInYear​(int year) {
+			
+		}
+		
+		public void generateRandomMovies​(int count) {
+			
+		}
+		
+		public void loadMovies() throws java.io.FileNotFoundException {
+		
+		}
+		
+		public void writeMoviesToFile() {
+		
+		}
+	}
