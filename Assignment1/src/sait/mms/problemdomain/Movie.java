17efc7ed5701d1cfa38d71937f6fe68b23744b56
Baodu <Baodu@DESKTOP-VVVDRG1>
2022-06-08 00:41:03 -0600

    sadasd

--- Assignment1/src/sait/mms/problemdomain/Movie.java
+++ Assignment1/src/sait/mms/problemdomain/Movie.java
@@ -2,4 +2,31 @@ package sait.mms.problemdomain;
 
 public class Movie {
 
+    private int duration;
+    private String title;
+    private int year;
+
+    public Movie(int duration, String title, int year) {
+
+    }
+    public String formatForFile() {
+
+    }
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
 }
