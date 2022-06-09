c5773ba069b43f2daf49db0e3b333cb8aa5c20b2
Baodu <Baodu@DESKTOP-VVVDRG1>
2022-06-09 17:15:36 -0600

    Movie.java works now

--- Assignment1/src/sait/mms/problemdomain/Movie.java
+++ Assignment1/src/sait/mms/problemdomain/Movie.java
@@ -1,32 +1,48 @@
 package sait.mms.problemdomain;
 
 public class Movie {
-
     private int duration;
     private String title;
     private int year;
 
     public Movie(int duration, String title, int year) {
+        this.duration = duration;
+        this.title = title;
+        this.year = year;
     }
-    
+
     public String formatForFile(int duration, String title, int year) {
-    	return toString();
+
+        return title;
     }
+
     public int getDuration() {
-    	return duration;
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public int getYear() {
         return year;
     }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     @Override
     public String toString() {
-		return new String();
+        return "Movie [duration=" + duration + ", title=" + title + ", year=" + year + "]";
     }
 
 }
